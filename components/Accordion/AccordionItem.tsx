e18b4e04b4e6aae08d83f77d1497d81a8e45ec3a
ho3einmolavi <ho3einmolavi00@gmail.com>
2024-12-09 05:36:34 +0330

    ChevronUp

--- components/Accordion/AccordionItem.tsx
+++ components/Accordion/AccordionItem.tsx
@@ -1,11 +1,11 @@
-// src/components/Accordion/AccordionItem.tsx
+import { ChevronUp } from "lucide-react";
 import React, { ReactNode } from "react";
 
 export interface AccordionItemProps {
   title: string;
   children: ReactNode;
-  isOpen?: boolean; // Made optional
-  onClick?: () => void; // Made optional
+  isOpen?: boolean;
+  onClick?: () => void;
 }
 
 const AccordionItem: React.FC<AccordionItemProps> = ({ title, children, isOpen = false, onClick }) => {
@@ -20,15 +20,7 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, children, isOpen =
           aria-controls={`accordion-content-${title}`}
         >
           <span>{title}</span>
-          <svg
-            className={`size-3 transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-            aria-hidden="true"
-          >
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
-          </svg>
+          <ChevronUp className={`transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`} />
         </button>
       </h2>
       {isOpen && (
