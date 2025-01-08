a8d2f50ab6d50f68525475b9f8ceaa5304640a38
ho3einmolavi <ho3einmolavi00@gmail.com>
2025-01-08 23:21:58 +0330

    improve login ux writing

--- pages/login.tsx
+++ pages/login.tsx
@@ -77,7 +77,7 @@ const LoginPage: NextPageWithLayout = () => {
               />
             </div>
             <div className="space-y-2">
-              <Label htmlFor="password">رمز عبور یا کد معرف را وارد کنید</Label>
+              <Label htmlFor="password">رمز عبور</Label>
               <Input {...register("password")} className="ltr" id="password" required type="password" />
             </div>
             <div className=" text-sm text-red-600">
